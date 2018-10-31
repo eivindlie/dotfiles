c5efe7ab2b0f015aa8b4ccf898f81673533f3236
Eivind Lie Andreassen <ela4991@gmail.com>
2018-10-31 12:13:39 +0100

    Change to use symbolic links

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 home = os.path.expanduser("~")
 
+
 def setup_git():
     name = input('Enter full name (for gitconfig): ')
     email = input('Enter email (for gitconfig): ')
@@ -10,11 +11,20 @@ def setup_git():
 
     gitconfig = gitconfig.format(name, email)
 
+    try:
+        os.rename(os.path.join(home, '.gitconfig'), os.path.join(home, '.gitconfig.bak'))
+    except WindowsError:
+        pass
+
     with open(os.path.join(home, '.gitconfig'), 'w') as f:
         f.write(gitconfig)
 
+    os.link('git/.gitconfig-symlink', os.path.join(home, '.gitconfig-symlink'))
+
+
 def main():
     setup_git()
+
 
 if __name__ == '__main__':
     main()
