f3c553b7582d7e0b6cc580d91ba77870648e2304
Eivind Lie Andreassen <ela4991@gmail.com>
2020-03-04 10:38:36 +0100

    Add basic .bashrc

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ import platform
 home = os.path.expanduser("~")
 system = platform.system()
 
+yes_answers = ('yes', 'YES', 'y', 'Y')
+
 def setup_git():
-    if input('Set up gitconfig? (yes/no) ') not in ('yes', 'YES', 'y', 'Y'):
+    if input('Set up gitconfig? (yes/no) ') not in yes_answers:
         return
     name = input('Enter full name (for gitconfig): ')
     email = input('Enter email (for gitconfig): ')
@@ -30,6 +32,21 @@ def setup_git():
 
     os.link('git/.gitconfig-symlink', os.path.join(home, '.gitconfig-symlink'))
 
+
+def setup_bashrc():
+    if input('Set up .bashrc? (yes/no) ') not in yes_answers:
+        return
+
+    if system == 'Windows':
+        print('You are running Windows - .bashrc cannot be setup')
+        return
+
+    try:
+        os.rename(os.path.join(home, '.bashrc'), os.path.join(home, '.bashrc-extra'))
+    except OSError:
+        pass
+
+    os.link('.bashrc', os.path.join(home, '.bashrc'))
 
 def main():
     setup_git()
