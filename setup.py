c3f46e267c9a66bb38b45014e464ba2fe6af7794
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2024-01-22 09:40:22 +0100

    Add .zshrc setup

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ from builtins import input
 
 import os
 import platform
+
 home = os.path.expanduser("~")
 system = platform.system()
 
 yes_answers = ('yes', 'YES', 'y', 'Y')
+
 
 def setup_git():
     if input('Set up gitconfig? (yes/no) ') not in yes_answers:
@@ -23,19 +25,24 @@ def setup_git():
 
     if system == 'Windows':
         try:
-            os.rename(os.path.join(home, '.gitconfig'), os.path.join(home, '.gitconfig.bak'))
+            os.rename(os.path.join(home, '.gitconfig'),
+                      os.path.join(home, '.gitconfig.bak'))
         except WindowsError:
             pass
     else:
         try:
-            os.rename(os.path.join(home, '.gitconfig'), os.path.join(home, '.gitconfig.bak'))
+            os.rename(os.path.join(home, '.gitconfig'),
+                      os.path.join(home, '.gitconfig.bak'))
         except OSError:
             pass
 
     with open(os.path.join(home, '.gitconfig'), 'w') as f:
         f.write(gitconfig)
 
-    os.symlink(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'git/.gitconfig-symlink'), os.path.join(home, '.gitconfig-symlink'))
+    os.symlink(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                     'git/.gitconfig-symlink'),
+        os.path.join(home, '.gitconfig-symlink'))
 
 
 def setup_bashrc():
@@ -47,24 +54,58 @@ def setup_bashrc():
         return
     try:
         with open(os.path.join(home, '.bashrc')) as f:
-            bashrc = f.read()
+            bashrc_content = f.read()
     except FileNotFoundError:
-        bashrc = ''
+        bashrc_content = ''
 
-    bashrc = 'source ~/.bashrc-symlink\n' + bashrc
+    bashrc_content = 'source ~/.bashrc-symlink\n' + bashrc_content
 
     with open(os.path.join(home, '.bashrc'), 'w') as f:
-        f.write(bashrc)
+        f.write(bashrc_content)
 
     try:
-        os.rename(os.path.join(home, '.bashrc-symlink'), os.path.join(home, '.bashrc-symlink.bak'))
+        os.rename(os.path.join(home, '.bashrc-symlink'),
+                  os.path.join(home, '.bashrc-symlink.bak'))
     except OSError:
         pass
 
-    os.symlink(os.path.join(os.path.dirname(os.path.abspath(__file__)), '.bashrc'), os.path.join(home, '.bashrc-symlink'))
+    os.symlink(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), '.bashrc'),
+        os.path.join(home, '.bashrc-symlink'))
+
+
+def setup_zshrc():
+    if input('Set up .zshrc? (yes/no) ') not in yes_answers:
+        return
+
+    if system == 'Windows':
+        print('You are running Windows - .zshrc cannot be setup')
+        return
+    try:
+        with open(os.path.join(home, '.zshrc')) as f:
+            zshrc_content = f.read()
+    except FileNotFoundError:
+        zshrc_content = ''
+
+    zshrc_content = 'source ~/.zshrc-symlink\n' + zshrc_content
+
+    with open(os.path.join(home, '.zshrc'), 'w') as f:
+        f.write(zshrc_content)
+
+    try:
+        os.rename(os.path.join(home, '.zshrc-symlink'),
+                  os.path.join(home, '.zshrc-symlink.bak'))
+    except OSError:
+        pass
+
+    os.symlink(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), '.zshrc'),
+        os.path.join(home, '.zshrc-symlink'))
+
 
 def main():
     setup_bashrc()
+    setup_zshrc()
     setup_git()
 
 
